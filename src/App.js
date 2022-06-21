0b01a59a65d9ed235e32ec5289238926a5000d76
Tiago <ztiagok@gmail.com>
2022-06-21 18:41:05 -0500

    c2 - Finalização do CSS

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
 import Missions from './components/Missions';
+import Footer from './components/Footer';
 import './App.css';
 
 class App extends React.Component {
@@ -12,6 +13,7 @@ class App extends React.Component {
         <Header />
         <SolarSystem />
         <Missions />
+        <Footer />
       </>
     );
   }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <div id="footer">
+        Desenvolvido por
+        {' '}
+        <b id="eu">Tiago Braga Costa </b>
+        {' '}
+        - Solar System Project
+      </div>
+    );
+  }
+}
+
+export default Footer;
